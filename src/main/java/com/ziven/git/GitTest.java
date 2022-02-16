1e7326f52835b59f35c358ca0500b47709c04c53
zivenko <2540702263@qq.com>
2022-02-16 15:53:25 +0800

    first commit

---
+++ src/main/java/com/ziven/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.ziven.git;
+
+/**
+ * @ClassName: GitTest
+ * @Description: 测试
+ * @author: ziven
+ * @date: 2022-02-16-15:49
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
